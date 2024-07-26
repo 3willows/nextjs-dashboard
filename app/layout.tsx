4038090f17d688cd054c93c5a4d37c9ae2ee5335
Jasper Wong <wlcjasper@gmail.com>
2024-07-26 16:30:48 +0100

    Fix typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body  className={`${inter.className} antialiased`}>{children}</body>
+      <body  className={`${inter.className} text-xl text-gray-800 md:text-3xl md:leading-normal antialiased`}>{children}</body>
     </html>
   );
 }
